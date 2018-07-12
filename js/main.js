68b630eef3b31d176065381756f0fa1634f0ef46
Ruslan Koshevoy <ruslan.koshevoj@gmail.com>
2018-07-12 10:48:39 +0300

    Yesturday I made some shit... Now I try to fix it

--- js/main.js
+++ js/main.js
@@ -1,82 +1,84 @@
 var calculator = new Vue({
   el: '#calculator',
+  data: {
+    result: '0'
+  },
   methods: {
     digit: function(buttonValue) {
-      var screen = document.getElementById('calculator-screen');
+      this.result = String(this.result);
 
-      if (screen.value == '0') {
-        screen.value = buttonValue;
+      if (this.result == '0') {
+        this.result = buttonValue;
       } else {
-        screen.value = screen.value + buttonValue;
+        this.result = this.result + buttonValue;
       }
     },
 
     clear: function() {
-      document.getElementById('calculator-screen').value = 0;
+      this.result = '0';
     },
 
     deleteLast: function() {
-      var screen = document.getElementById('calculator-screen');
+      this.result = this.result.slice(0, -1);
 
-      screen.value = screen.value.slice(0, -1);
-
-      if (screen.value == '')
-        screen.value = '0';
+      if (this.result == '')
+        this.result = '0';
     },
 
     operation: function(operationSymbol) {
-      var screen = document.getElementById('calculator-screen');
+      this.result = String(this.result);
 
-      if (screen.value.slice(-1) == '*' || screen.value.slice(-1) == '/' || screen.value.slice(-1) == '-' || screen.value.slice(-1) == '+') {
-        screen.value = screen.value.slice(0, -1);
+      if (this.result.slice(-1) == '*' || this.result.slice(-1) == '/' || this.result.slice(-1) == '-' || this.result.slice(-1) == '+') {
+        this.result = this.result.slice(0, -1);
       }
 
-      screen.value = screen.value + operationSymbol;
+      var digitsArray = this.result.split('');
 
-      var digitsArray = screen.value.split('');
+      this.result = this.result + operationSymbol;
 
       if (operationSymbol == '=') {
-        result();
+        this.getResult();
       } else {
         for (var i = 1; i < digitsArray.length - 1; i++) {
           if (digitsArray[i] == '*' || digitsArray[i] == '/' || digitsArray[i] == '-' || digitsArray[i] == '+') {
-            result();
+            this.getResult();
           }
         }
       }
+    },
 
-      function result() {
-        var firstValue, secondValue;
+    getResult: function() {
+      var firstValue, secondValue;
+      var digitsArray = this.result.split('');
 
-        screen.value = screen.value.slice(0, -1);
+      this.result = this.result.slice(0, -1);
 
-        function valuesSplit(i) {
-          firstValue = digitsArray.slice(0, i).join('');
-          secondValue = digitsArray.slice(i + 1, digitsArray.length - 1).join('');
-        }
+      function valuesSplit(i) {
+        firstValue = digitsArray.slice(0, i).join('');
+        secondValue = digitsArray.slice(i + 1, digitsArray.length - 1).join('');
+      }
 
-        for (var i = 0; i < digitsArray.length - 1; i++) {
+      for (var i = 0; i < digitsArray.length - 1; i++) {
 
-          switch (digitsArray[i]) {
-            case '*':
-              valuesSplit(i);
-              screen.value = parseFloat(firstValue) * parseFloat(secondValue);
-              break;
-            case '/':
-              valuesSplit(i);
-              screen.value = parseFloat(firstValue) / parseFloat(secondValue);
-              break;
-            case '-':
-              valuesSplit(i);
-              screen.value = parseFloat(firstValue) - parseFloat(secondValue);
-              break;
-            case '+':
-              valuesSplit(i);
-              screen.value = parseFloat(firstValue) + parseFloat(secondValue);
-              break;
-            default:
-              break;
-          }
+        switch (digitsArray[i]) {
+          case '*':
+            valuesSplit(i);
+            this.result = parseFloat(firstValue) * parseFloat(secondValue);
+            break;
+          case '/':
+            valuesSplit(i);
+            this.result = parseFloat(firstValue) / parseFloat(secondValue);
+            break;
+          case '-':
+            valuesSplit(i);
+            this.result = parseFloat(firstValue) - parseFloat(secondValue);
+            break;
+          case '+':
+            valuesSplit(i);
+            this.result = parseFloat(firstValue) + parseFloat(secondValue);
+            break;
+          default:
+            break;
         }
       }
     }
